4008a97ffb52a65fe4ff2b15cc8c77be09b7229c
eignatik <light.ji@yandex.ru>
2016-10-03 16:06:14 -0400

    values have been calibrated

--- src/ModelingExample.java
+++ src/ModelingExample.java
@@ -1,7 +1,8 @@
 public class ModelingExample {
     public static void main(String[] args) {
         Model model = new Model();
-        model.setPrintStep(7);
+        model.setPrintStep(20);
+        model.setStep(0.05);
         model.showResults();
     }
 }
